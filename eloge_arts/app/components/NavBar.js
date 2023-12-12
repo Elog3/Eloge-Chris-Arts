514ae7a92477cd1c6e583b46191f095b60ea6098
0122345 <ntwarifiacre043@gmail.com>
2023-12-13 00:01:20 +0200

    structuring

---
+++ eloge_arts/app/components/NavBar.js
@@ -0,0 +1,32 @@
+import { img } from '../eloge-arts.ico';
+ 
+
+export const NavBar = () => {
+  return (
+    <div className="flex float-left left-0 top-0 w-[45vh] bg-[#222629] h-screen">
+
+        <div className="">
+            <span>
+                <img src="./app/eloge-arts.ico" alt="icon" className='flex float-left w-8'/>
+            </span>
+
+
+            <span className='inline justify-between text-white font-bold float-right px-[8pc] w-9'>
+                <span>
+                </span>
+                <p>
+                    Eloge Arts
+                </p>
+            </span>
+            
+        </div>
+
+
+        <div className=""></div>
+
+
+        <div className=""></div>
+        
+    </div>
+  )
+}
