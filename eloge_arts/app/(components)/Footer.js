f34a1a3fb844ddb73d3f8793e2205c9e52061d99
0122345 <ntwarifiacre043@gmail.com>
2023-12-13 23:43:41 +0200

    structuring

---
+++ eloge_arts/app/(components)/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='flex flex-col bg-[#DCE3E2] h-[200px]'>
+        <div className="flex flex-row justify-center items-center bottom-0  text-black text-xl bold ">
+         <span className="bottom-3 ">©Untitled. All rights reserved.</span>
+          |
+         <span className="">Designer: Eloge_Arts</span>
+         </div>
+    </div>
+  )
+}
